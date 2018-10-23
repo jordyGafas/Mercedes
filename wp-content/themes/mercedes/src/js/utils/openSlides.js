d403963362c275f6d80443242f93c4d684437551
Jordy <jordy@gafas.be>
2018-10-23 16:39:23 +0200

    font-update open sans

--- wp-content/themes/mercedes/src/js/utils/openSlides.js
+++ wp-content/themes/mercedes/src/js/utils/openSlides.js
@@ -2,22 +2,39 @@ import "@babel/polyfill";
 //import "nodelist-foreach-polyfill";
 
 export const openSlides = () => {
-  const slider = document.querySelector(".slide-open-container1");
-  const button = document.querySelector(
+  var slider = document.querySelector(".slide-open-container1");
+  var button = document.querySelector(
     ".slide-open-container1 .slide-open-toggle"
   );
-  const closedContent = document.querySelector(
+  var closedContent = document.querySelector(
     ".slide-open-container1 .slide-open-content"
   );
-  const openContent = document.querySelector(
+  var openContent = document.querySelector(
     ".slide-open-container1 .slide-open-content-inner-container"
   );
-  console.log("button", button)
+  console.log("button", button);
   if (button != null) {
     button.addEventListener("click", function() {
-     closedContent.classList.toggle("closed");
-     openContent.classList.toggle("closed");
-     button.classList.toggle("closed");
+      closedContent.classList.toggle("closed");
+      openContent.classList.toggle("closed");
+      button.classList.toggle("closed");
+    });
+  }
+
+  var slider2 = document.querySelector(".slide-open-container2");
+  var button2 = document.querySelector(".slide-open-container2 .slide-open-toggle");
+  var closedContent2 = document.querySelector(
+    ".slide-open-container2 .slide-open-content"
+  );
+  var openContent2 = document.querySelector(
+    ".slide-open-container2 .slide-open-content-inner-container"
+  );
+  console.log("button", button2);
+  if (button2 != null) {
+    button2.addEventListener("click", function() {
+      closedContent2.classList.toggle("closed");
+      openContent2.classList.toggle("closed");
+      button2.classList.toggle("closed");
     });
   }
 };
