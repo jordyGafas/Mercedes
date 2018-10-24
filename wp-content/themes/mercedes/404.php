017c5447d2544ecf82d72c448a7d6443c31d7f2c
Jordy <jordy@gafas.be>
2018-10-24 11:01:18 +0200

    small css fixes

--- wp-content/themes/mercedes/404.php
+++ wp-content/themes/mercedes/404.php
@@ -9,10 +9,15 @@ get_header(); ?>
 
 <div class="error-container">
 	<div class="error-container__inner">
-		<h1><?php the_field('404_title', 'option'); ?></h1>
-		<a href="<?php echo home_url(); ?>">&#60; <?php the_field('label_back', 'option'); ?></a>
+		<h1><?php echo 'It looks like nothing was found at this location.'; ?></h1>
 	</div>
 	<div class="error-container__image u-full u-cover--center lazy" data-src="<?php the_field('404_image', 'option'); ?>"></div>
+	<div class="single-article__back">
+		<div class="back-row">
+			<a href="<?php echo get_site_url();?>"><div>Back to home</div></a>
+		</div>
+	</div>
+</div>
 </div>
 
 <?php get_footer(); ?>
